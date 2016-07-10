25669fe60085022c3435c751ad40584fc1e9fd0e
DAB0mB <emanor6@gmail.com>
2016-07-11 00:29:57 +0300

    Step 3.2: Reset tab roots in tabs components

--- app/pages/tabs/tabs.js
+++ app/pages/tabs/tabs.js
@@ -1,7 +1,6 @@
-import {Component} from '@angular/core'
-import {HomePage} from '../home/home';
-import {AboutPage} from '../about/about';
-import {ContactPage} from '../contact/contact';
+import {Component} from '@angular/core';
+import {ChatsPage} from '../chats/chats';
+
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -10,8 +9,6 @@ export class TabsPage {
   constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
-    this.tab1Root = HomePage;
-    this.tab2Root = AboutPage;
-    this.tab3Root = ContactPage;
+    this.chatsTabRoot = ChatsPage;
   }
 }
