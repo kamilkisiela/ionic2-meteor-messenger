887e923e502e64f6e27e11345a91f2d7de22aeb7
DAB0mB <emanor6@gmail.com>
2016-07-11 23:08:05 +0300

    Step 4.10: Add meteor packages webpack handler

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,19 @@ module.exports = {
 };
 
 function resolveExternals(context, request, callback) {
-  return cordovaPlugin(request, callback) ||
+  return meteorPack(request, callback) ||
+         cordovaPlugin(request, callback) ||
          callback();
+}
+
+function meteorPack(request, callback) {
+  var match = request.match(/^meteor\/(.+)$/);
+  var pack = match && match[1];
+
+  if (pack) {
+    callback(null, 'window.Package && Package["' + pack + '"]');
+    return true;
+  }
 }
 
 function cordovaPlugin(request, callback) {
