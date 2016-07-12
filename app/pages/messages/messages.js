18e5b17eed18c8d5565d12c67514d469fa9700e7
DAB0mB <emanor6@gmail.com>
2016-07-12 23:36:29 +0300

    Step 5.3: Add basic messages component

---
+++ app/pages/messages/messages.js
@@ -0,0 +1,41 @@
+import {Component} from '@angular/core';
+import {NavParams} from 'ionic-angular';
+import {MeteorComponent} from 'angular2-meteor';
+import {Messages} from 'api/collections';
+
+
+@Component({
+  templateUrl: 'build/pages/messages/messages.html'
+})
+export class MessagesPage extends MeteorComponent {
+  static parameters = [[NavParams]]
+
+  constructor(navParams) {
+    super();
+
+    this.isEven = false;
+    this.activeChat = navParams.get('chat');
+
+    this.title = this.activeChat.title;
+    this.picture = this.activeChat.picture;
+
+    this.autorun(() => {
+      this.messages = this.findMessages();
+    }, true);
+  }
+
+  findMessages() {
+    return Messages.find({
+      chatId: this.activeChat._id
+    }, {
+      sort: {createdAt: 1},
+      transform: this::this.transformMessage
+    });
+  }
+
+  transformMessage(message) {
+    message.ownership = this.isEven ? 'mine' : 'other';
+    this.isEven = !this.isEven;
+    return message;
+  }
+}
