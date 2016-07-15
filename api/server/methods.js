9c813fdcabde82707ae7064fae4d5fb2622f2783
DAB0mB <emanor6@gmail.com>
2016-07-15 22:35:12 +0300

    Step 8.2: Add removeChat() method to methods.js file

--- api/server/methods.js
+++ api/server/methods.js
@@ -46,6 +46,21 @@ Meteor.methods({
     Chats.insert(chat);
   },
 
+  removeChat(chatId) {
+    if (!this.userId) throw new Meteor.Error('unauthorized',
+      'User must be logged-in to remove chat');
+
+    check(chatId, nonEmptyString);
+
+    const chatExists = !!Chats.find(chatId).count();
+
+    if (!chatExists) throw new Meteor.Error('chat-not-exists',
+      'Chat doesn\'t exist');
+
+    Messages.remove({chatId});
+    Chats.remove(chatId);
+  },
+
   addMessage(chatId, content) {
     if (!this.userId) throw new Meteor.Error('unauthorized',
       'User must be logged-in to create a new chat');
