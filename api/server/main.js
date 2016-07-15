c1d380fa2d63e66a2c6b37f02450c3cce78c17c5
DAB0mB <emanor6@gmail.com>
2016-07-15 18:12:45 +0300

    Step 7.8: Fabricate real users on bootstrap

--- api/server/main.js
+++ api/server/main.js
@@ -1,65 +1,48 @@
-import Moment from 'moment';
 import {Meteor} from 'meteor/meteor';
+import {Accounts} from 'meteor/accounts-base';
 import {Chats, Messages} from './collections';
 
 
-Meteor.startup(() => {
-  if (Chats.find().count()) return;
+Meteor.startup(function() {
+  if (Meteor.users.find().count()) return;
 
-  let chatId;
-
-  chatId = Chats.insert({
-    title: 'Ethan Gonzalez',
-    picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg'
+  Accounts.createUserWithPhone({
+    phone: '+972540000001',
+    profile: {
+      name: 'Ethan Gonzalez',
+      picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg'
+    }
   });
 
-  Messages.insert({
-    chatId: chatId,
-    content: 'You on your way?',
-    createdAt: Moment().subtract(1, 'hours').toDate()
+  Accounts.createUserWithPhone({
+    phone: '+972540000002',
+    profile: {
+      name: 'Bryan Wallace',
+      picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg'
+    }
   });
 
-  chatId = Chats.insert({
-    title: 'Bryan Wallace',
-    picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg'
-  });
-
-  Messages.insert({
-    chatId: chatId,
-    content: 'Hey, it\'s me',
-    createdAt: Moment().subtract(2, 'hours').toDate()
-  });
-
-  chatId = Chats.insert({
-    title: 'Avery Stewart',
+  Accounts.createUserWithPhone({
+    phone: '+972540000003',
+    profile: {
+      name: 'Avery Stewart',
       picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg'
+    }
   });
 
-  Messages.insert({
-    chatId: chatId,
-    content: 'I should buy a boat',
-    createdAt: Moment().subtract(1, 'days').toDate()
-  });
-
-  chatId = Chats.insert({
-    title: 'Katie Peterson',
+  Accounts.createUserWithPhone({
+    phone: '+972540000004',
+    profile: {
+      name: 'Katie Peterson',
       picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg'
+    }
   });
 
-  Messages.insert({
-    chatId: chatId,
-    content: 'Look at my mukluks!',
-    createdAt: Moment().subtract(4, 'days').toDate()
-  });
-
-  chatId = Chats.insert({
-    title: 'Ray Edwards',
-    picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg'
-  });
-
-  Messages.insert({
-    chatId: chatId,
-    content: 'This is wicked good ice cream.',
-    createdAt: Moment().subtract(2, 'weeks').toDate()
+  Accounts.createUserWithPhone({
+    phone: '+972540000005',
+    profile: {
+      name: 'Ray Edwards',
+      picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg'
+    }
   });
 });
