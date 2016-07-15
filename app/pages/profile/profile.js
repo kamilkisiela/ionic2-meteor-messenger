933fdccca34d8f06d8833c4a5d9f10d0ae47ddd1
DAB0mB <emanor6@gmail.com>
2016-07-15 16:05:36 +0300

    Step 6.16: Add profile component

---
+++ app/pages/profile/profile.js
@@ -0,0 +1,43 @@
+import {Component} from '@angular/core';
+import {NavController, Alert} from 'ionic-angular';
+import {MeteorComponent} from 'angular2-meteor';
+import {Meteor} from 'meteor/meteor';
+import {TabsPage} from '../tabs/tabs';
+
+
+@Component({
+  templateUrl: 'build/pages/profile/profile.html'
+})
+export class ProfilePage extends MeteorComponent {
+  static parameters = [[NavController]]
+
+  constructor(navCtrl) {
+    super();
+
+    this.navCtrl = navCtrl;
+
+    this.profile = Meteor.user().profile || {
+      name: '',
+      picture: '/ionicons/svg/ios-contact.svg'
+    };
+  }
+
+  done() {
+    this.call('updateProfile', this.profile, (e) => {
+      if (e) return this.handleError(e);
+      this.navCtrl.push(TabsPage);
+    }, true);
+  }
+
+  handleError(e) {
+    console.error(e);
+
+    const alert = Alert.create({
+      title: 'Oops!',
+      message: e.message,
+      buttons: ['OK']
+    });
+
+    this.navCtrl.present(alert);
+  }
+}
