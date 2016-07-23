38b69905f4a024b5779d6e1573c483ffc9a51f6a
DAB0mB <emanor6@gmail.com>
2016-07-23 22:44:45 +0300

    Step 2.8: Add basic chat and message models definitions

---
+++ typings/models.d.ts
@@ -0,0 +1,14 @@
+declare module 'api/models' {
+  interface Chat {
+    _id?: string;
+    title?: string;
+    picture?: string;
+    lastMessage?: Message;
+  }
+
+  interface Message {
+    _id?: string;
+    content?: string;
+    createdAt?: Date;
+  }
+}
