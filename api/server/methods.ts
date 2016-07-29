a91e6af019212cbf2d31da0ded8ed1c4496b1d15
DAB0mB <emanor6@gmail.com>
2016-07-29 12:15:23 +0300

    Step 4.16: Add validations to 'addMessage()' method in api

--- api/server/methods.ts
+++ api/server/methods.ts
@@ -1,9 +1,18 @@
 import {Meteor} from 'meteor/meteor';
+import {check, Match} from 'meteor/check';
 import {Chats, Messages} from './collections';
+
+const nonEmptyString = Match.Where((str) => {
+  check(str, String);
+  return str.length > 0;
+});
 
 
 Meteor.methods({
   addMessage(chatId: string, content: string): void {
+    check(chatId, nonEmptyString);
+    check(content, nonEmptyString);
+
     const chatExists = !!Chats.find(chatId).count();
 
     if (!chatExists) throw new Meteor.Error('chat-not-exists',
