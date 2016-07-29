e0c113be8dfb09f1369ca2f3bdf076cb1d0f1ba4
DAB0mB <emanor6@gmail.com>
2016-07-29 14:35:12 +0300

    Step 5.3: Install 'meteor-accounts-phone' typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="globals/meteor-accounts-phone/index.d.ts" />
 /// <reference path="globals/meteor/index.d.ts" />
 /// <reference path="models.d.ts" />
